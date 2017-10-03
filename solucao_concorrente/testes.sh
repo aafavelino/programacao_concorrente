0685b307ef6d6aa82dbc746f3f6f6cba492aa0ae
Adelino Afonso <aafavelino@icloud.com>
2017-10-03 19:16:17 -0300

    fim

--- solucao_concorrente/testes.sh
+++ solucao_concorrente/testes.sh
@@ -1,20 +1,20 @@
 echo "Matriz 4x4"
 ./bin/exe 4 4 ../matrizes/A4x4.txt ../matrizes/B4x4.txt
 echo "Matriz 8x8"
-./bin/exe 8 4 ../matrizes/A8x8.txt ../matrizes/B8x8.txt
+./bin/exe 8 8 ../matrizes/A8x8.txt ../matrizes/B8x8.txt
 echo "Matriz 16x16"
-./bin/exe 16 4 ../matrizes/A16x16.txt ../matrizes/B16x16.txt
+./bin/exe 16 8 ../matrizes/A16x16.txt ../matrizes/B16x16.txt
 echo "Matriz 32x32"
-./bin/exe 32 4 ../matrizes/A32x32.txt ../matrizes/B32x32.txt
+./bin/exe 32 8 ../matrizes/A32x32.txt ../matrizes/B32x32.txt
 echo "Matriz 64x64"
-./bin/exe 64 4 ../matrizes/A64x64.txt ../matrizes/B64x64.txt
+./bin/exe 64 8 ../matrizes/A64x64.txt ../matrizes/B64x64.txt
 echo "Matriz 128x128"
-./bin/exe 128 4 ../matrizes/A128x128.txt ../matrizes/B128x128.txt
+./bin/exe 128 8 ../matrizes/A128x128.txt ../matrizes/B128x128.txt
 echo "Matriz 256x256"
-./bin/exe 256 4 ../matrizes/A256x256.txt ../matrizes/B256x256.txt
+./bin/exe 256 8 ../matrizes/A256x256.txt ../matrizes/B256x256.txt
 echo "Matriz 512x512"
-./bin/exe 512 4 ../matrizes/A512x512.txt ../matrizes/B512x512.txt
+./bin/exe 512 8 ../matrizes/A512x512.txt ../matrizes/B512x512.txt
 echo "Matriz 1024x1024"
-./bin/exe 1024 4 ../matrizes/A1024x1024.txt ../matrizes/B1024x1024.txt
+./bin/exe 1024 8 ../matrizes/A1024x1024.txt ../matrizes/B1024x1024.txt
 echo "Matriz 2048x2048"
-./bin/exe 2048 4 ../matrizes/A2048x2048.txt ../matrizes/B2048x2048.txt
+./bin/exe 2048 8 ../matrizes/A2048x2048.txt ../matrizes/B2048x2048.txt
